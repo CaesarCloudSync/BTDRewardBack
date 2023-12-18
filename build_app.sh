9b979271e4d74b9e33fb4d552847a8a78d3dac1a
amari <amari.lawal@gmail.com>
2023-12-18 21:34:15 +0000

    invite a friend works just needs testing

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,3 @@
 git add .
 git commit -m "$1"
-git push origin master:master
-docker build -t palondomus/blacktechdivisionreward:latest .
-docker push palondomus/blacktechdivisionreward:latest
-docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:latest
+git push origin beforeinviteafriend:beforeinviteafriend
