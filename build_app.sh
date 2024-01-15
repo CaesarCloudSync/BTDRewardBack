bdd031fc9a87c6e969e1d3f77213b10876d8e840
amari <amari.lawal@gmail.com>
2024-01-15 17:22:19 +0000

    show kartra event data

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin master:master
-docker build -t palondomus/blacktechdivisionreward:latest .
-docker push palondomus/blacktechdivisionreward:latest
-docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:latest
+docker build -t palondomus/blacktechdivisionreward:newest .
+docker push palondomus/blacktechdivisionreward:newest
+docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:newest
