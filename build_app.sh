d57c05e0ff85a09dd9d773e6bafed7fc20d79798
amari <amari.lawal@gmail.com>
2024-01-15 20:04:08 +0000

    checking if action has been taken before to avoid double payement, turned to base64 to get data back test

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin master:master
-docker build -t palondomus/blacktechdivisionreward:newest .
-docker push palondomus/blacktechdivisionreward:newest
-docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:newest
+docker build -t palondomus/blacktechdivisionreward:finest .
+docker push palondomus/blacktechdivisionreward:finest
+docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:finest
