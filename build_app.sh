0ef048dc800b1447e5d988c2e4f55f7d2073b533
amari <amari.lawal@gmail.com>
2024-02-02 21:22:11 +0000

    implementing auto auth redirect

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
-git push origin authidkartra:authidkartra
-docker build -t palondomus/blacktechdivisionreward:authidkartra .
-docker push palondomus/blacktechdivisionreward:authidkartra
-docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:authidkartra
+git push origin master:master
+docker build -t palondomus/blacktechdivisionreward:latest .
+docker push palondomus/blacktechdivisionreward:latest
+docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:latest
