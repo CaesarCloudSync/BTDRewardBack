d9ec5115f876dfabd50d4c7c915febf294c4a1a2
amari <amari.lawal@gmail.com>
2024-02-06 23:56:15 +0000

    trying terraform

--- build_app.sh
+++ build_app.sh
@@ -3,15 +3,14 @@ function getVersions() {
     IN=$(cat main.tf | grep palondomus/blacktechdivisionreward)
     arrIN=(${IN//:/ })
     oldv=$((${arrIN[3]::-1}))
-    oldstr="blacktechdivisionreward:$oldv"
-    newstr="blacktechdivisionreward:$(($oldv+1))"
-    echo $oldstr $newstr
+    newv=$(($oldv+1))
+    echo $oldv $newv
 }
 
 git add .
 git commit -m "$1"
-read -r oldstr newstr  <<< $(getVersions)
-sed -i -e "s/$oldstr/$newstr/" main.tf
+read -r oldv newv  <<< $(getVersions)
+sed -i -e "s/blacktechdivisionreward:$oldv/blacktechdivisionreward:$newv/" main.tf
 git push origin -u terraformbtd:terraformbtd
 docker build -t palondomus/blacktechdivisionreward:$newv .
 docker push palondomus/blacktechdivisionreward:$newv
