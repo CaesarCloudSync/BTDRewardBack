239e7e25477a407f3a2f79c40b93578db6418f1e
amari <amari.lawal@gmail.com>
2024-02-06 23:58:28 +0000

    trying terraform again

--- build_app.sh
+++ build_app.sh
@@ -7,15 +7,20 @@ function getVersions() {
     echo $oldv $newv
 }
 
-git add .
-git commit -m "$1"
+# Change Docker tag in .tf
 read -r oldv newv  <<< $(getVersions)
 sed -i -e "s/blacktechdivisionreward:$oldv/blacktechdivisionreward:$newv/" main.tf
+
+# Push Github
+git add .
+git commit -m "$1"
 git push origin -u terraformbtd:terraformbtd
+
+# Push Docker
 docker build -t palondomus/blacktechdivisionreward:$newv .
 docker push palondomus/blacktechdivisionreward:$newv
 
-
+# Terraform Push Google Cloud
 terraform init
 terraform plan 
 terraform apply -auto-approve
--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "google_cloud_run_service" "run_service" {
   template {
     spec {
       containers {
-        image = "palondomus/blacktechdivisionreward:5"
+        image = "palondomus/blacktechdivisionreward:7"
       }
     }
   }
