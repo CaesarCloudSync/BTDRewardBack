bae304521d0a10d3c4c4d6963bf3817f861fc4f6
amari <amari.lawal@gmail.com>
2024-02-06 19:18:13 +0000

    debugging Nonetype error for tagging

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin master:master
-docker build -t palondomus/blacktechdivisionreward:finest .
-docker push palondomus/blacktechdivisionreward:finest
-docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:finest
+docker build -t palondomus/blacktechdivisionreward:latest .
+docker push palondomus/blacktechdivisionreward:latest
+docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:latest
