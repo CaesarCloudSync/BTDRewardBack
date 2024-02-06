959de4ac8c7215fac7e6580e66e35929eb1e2dd7
amari <amari.lawal@gmail.com>
2024-02-06 19:40:13 +0000

    trying to debug NoneType error

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin master:master
-docker build -t palondomus/blacktechdivisionreward:latest .
-docker push palondomus/blacktechdivisionreward:latest
-docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:latest
+docker build -t palondomus/blacktechdivisionreward:bestest .
+docker push palondomus/blacktechdivisionreward:bestest
+docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:bestest
