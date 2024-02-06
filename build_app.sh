88a0e30d5805dfbf414ed338ee2328f81473d461
amari <amari.lawal@gmail.com>
2024-02-06 21:43:13 +0000

    trying terraform

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 git add .
 git commit -m "$1"
-git push origin master:master
+git push origin terraformbtd:terraformbtd
 docker build -t palondomus/blacktechdivisionreward:bestest .
 docker push palondomus/blacktechdivisionreward:bestest
-docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:bestest
+terraform init
+terraform plan
+url=$(terraform apply) | grep blacktechdivisionreward
+curl "${url/service_url = /""}"  
+#docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:bestest
