7fe97082d3110e48cb4f5fe3c5e636bc43ea0ae4
amari <amari.lawal@gmail.com>
2024-02-06 21:50:20 +0000

    trying terraform

--- build_app.sh
+++ build_app.sh
@@ -4,9 +4,9 @@ git commit -m "$1"
 git push origin -u terraformbtd:terraformbtd
 docker build -t palondomus/blacktechdivisionreward:bestest .
 docker push palondomus/blacktechdivisionreward:bestest
-terraform destroy
+#terraform destroy
 terraform init
-terraform plan
+terraform plan -o
 url=$($(terraform apply) | grep blacktechdivisionreward)
 echo $url
 curl "${url/service_url = /""}"  
