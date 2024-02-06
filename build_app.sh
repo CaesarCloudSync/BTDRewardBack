4d2b6d7ef3683c163cb051abe01657539cf76a24
amari <amari.lawal@gmail.com>
2024-02-06 23:54:30 +0000

    trying terraform

--- build_app.sh
+++ build_app.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+function getVersions() {
+    IN=$(cat main.tf | grep palondomus/blacktechdivisionreward)
+    arrIN=(${IN//:/ })
+    oldv=$((${arrIN[3]::-1}))
+    oldstr="blacktechdivisionreward:$oldv"
+    newstr="blacktechdivisionreward:$(($oldv+1))"
+    echo $oldstr $newstr
+}
 
 git add .
 git commit -m "$1"
+read -r oldstr newstr  <<< $(getVersions)
+sed -i -e "s/$oldstr/$newstr/" main.tf
 git push origin -u terraformbtd:terraformbtd
-docker build -t palondomus/blacktechdivisionreward:$2 .
-docker push palondomus/blacktechdivisionreward:$2
+docker build -t palondomus/blacktechdivisionreward:$newv .
+docker push palondomus/blacktechdivisionreward:$newv
+
+
 terraform init
-terraform plan # -o
+terraform plan 
 terraform apply -auto-approve
-docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:$2
+docker run -it -p 8080:8080 palondomus/blacktechdivisionreward:$newv
+
+
+
+
+
+
+
